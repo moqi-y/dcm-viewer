72adb3e1fe67613b79a1936029f0f338fb9d4453
yuanzhen <yuanzhenwork@qq.com>
2025-06-04 14:58:43 +0800

    feat(Viewport): 引入并配置cornerstoneTools工具

--- src/components/Viewport.vue
+++ src/components/Viewport.vue
@@ -6,12 +6,21 @@
 import { onMounted } from 'vue';
 import { RenderingEngine, Enums, init as coreInit } from '@cornerstonejs/core';
 import { init as dicomImageLoaderInit } from '@cornerstonejs/dicom-image-loader';
+import {
+    init as cornerstoneToolsInit,
+    ToolGroupManager,
+    WindowLevelTool,
+    ZoomTool,
+    Enums as csToolsEnums,
+    addTool,
+} from '@cornerstonejs/tools'; // 引入 cornerstoneTools
 
 
 const init = async () => {
     // 初始化 CornerstoneJS 和加载器
     await coreInit();
     await dicomImageLoaderInit();
+    await cornerstoneToolsInit();
 
     // 获取元素
     const element = document.getElementById('viewer');
@@ -38,9 +47,46 @@ const init = async () => {
     const imageId = 'wadouri:http://127.0.0.1:8080/image_5';
     const imageIds = [imageId]; // 将单个图像 ID 放入数组
 
+    /** 添加窗位窗宽工具 */
+    element.oncontextmenu = (e) => e.preventDefault(); // 阻止默认的右键菜单
+    const toolGroupId = 'ctToolGroup';
+    const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
+    addTool(ZoomTool);
+    addTool(WindowLevelTool);
+    toolGroup.addTool(ZoomTool.toolName);
+    toolGroup.addTool(WindowLevelTool.toolName);
+
+    toolGroup.addViewport(viewportId, renderingEngineId);
+    console.log("csToolsEnums.MouseBindings", csToolsEnums);
+
+    /**
+     * Primary: 鼠标左键
+     * Secondary: 鼠标右键
+     * Auxiliary: 鼠标中键
+     * Wheel: 鼠标滚轮
+     * Primary_And_Secondary：鼠标左键和右键同时按下
+     */
+    // 缩放工具
+    toolGroup.setToolActive(ZoomTool.toolName, {
+        bindings: [
+            {
+                mouseButton: csToolsEnums.MouseBindings.Wheel, // 鼠标滚轮
+            },
+        ]
+    });
+    // 窗宽窗位工具
+    toolGroup.setToolActive(WindowLevelTool.toolName, {
+        bindings: [
+            {
+                mouseButton: csToolsEnums.MouseBindings.Primary,  // 鼠标左键
+            },
+        ]
+    });
+
     // 设置图像堆栈并渲染
     viewport.setStack(imageIds, 0); // 设置堆栈并显示第 0 张图像
     viewport.render();
+
 }
 
 onMounted(() => {
