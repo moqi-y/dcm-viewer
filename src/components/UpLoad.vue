183666be568dd666e0e7cc75f73a48f9075704b4
yuanzhen <yuanzhenwork@qq.com>
2025-06-06 12:51:07 +0800

    style(UpLoad.vue): 优化上传按钮样式及布局

--- src/components/UpLoad.vue
+++ src/components/UpLoad.vue
@@ -1,8 +1,6 @@
 <template>
-    <!-- <div>
-        <input type="file" multiple ref="inputRef" @change="handleChange" />
-    </div> -->
     <label class="custom-upload-button">
+      <img style="width: 20px;height: 18px;margin:0 2px 2px 0;" src="/icon/上传_upload.svg" alt="Upload" class="upload-icon" />
         <input type="file" multiple ref="inputRef" @change="handleChange" class="hidden-input" />
         上传文件
     </label>
@@ -24,13 +22,13 @@ const handleChange = (event) => {
 
 <style scoped>
 .custom-upload-button {
-  display: inline-block;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100px;
   height: 40px;
   background: rgb(80, 165, 245);
   color: white;
-  text-align: center;
-  line-height: 40px;
   font-size: 14px;
   cursor: pointer;
   border-radius: 5px;
